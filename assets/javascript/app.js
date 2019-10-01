987e2dc7169250d071e0a31e2f7ca9cccd1ad613
Khoa Nguyen <kh.nguyen0101@gmail.com>
2019-10-01 14:42:39 -0700

    added ajax response and gif switch function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,63 +7,127 @@
 // GIPHY Parameters: q, limit, rating
 
 // Create all variables
-var celebrities = [
-    "Frank Ocean", 
-    "Kanye West", 
-    "The Weeknd", 
-    "Post Malone"
-];
+var celebrities = ["Frank Ocean", "Kanye West", "The Weeknd", "Post Malone"];
 
-var gifQueryStr = "";
-var gifQueryRating = "";
-var gifQueryLimit;
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5H94PgMQxCIRZsyDmM1iOJzMsHdIhm8S&q=" + gifQueryStr + "&limit=" + gifQueryLimit + "&rating=" + gifQueryRating; 
-
+// Get a handle on the API Response
 var gifResponse;
 
 // Create a function that will render all the buttons listed in the topics array
 function renderButtons() {
-    // Delete child nodes as to not repeat celebrity names
-    $("#buttons-display").empty();
+  // Delete child nodes as to not repeat celebrity names
+  $("#buttons-display").empty();
 
-    // Loop through celebs array to dynamically create a button for each one
-    for (var i = 0; i < celebrities.length; i++) {
-        var newCelebButton = $("<button>");
+  // Loop through celebs array to dynamically create a button for each one
+  for (var i = 0; i < celebrities.length; i++) {
+    var newCelebButton = $("<button>");
 
-        // add a class of "celeb" to that new button
-        newCelebButton.addClass("celeb");
+    // add a class of "celeb" to that new button
+    newCelebButton.addClass("button");
 
-        // add a data-attribute with the value of the celeb at index i
-        newCelebButton.attr("celeb_num_" + celebrities[i]);
+    // add a data-attribute with the value of the celeb at index i
+    newCelebButton.attr("data-celeb", celebrities[i]);
 
-        // add text to the new button with the value of the celeb at index i
-        newCelebButton.text(celebrities[i]);
+    // add text to the new button with the value of the celeb at index i
+    newCelebButton.text(celebrities[i]);
 
-        // add the newly created button to the HTML DOM
-        $("#buttons-display").append(newCelebButton);
-    }
-};
+    // add the newly created button to the HTML DOM
+    $("#buttons-display").append(newCelebButton);
+    console.log("celeb appended");
+  }
+
+    // Create an event listener for whenever the celeb name buttons are clicked
+    $(".button").on("click", function() {
+        event.preventDefault();
+      
+        // Clear the display 
+        $("#gif-display").empty();
+      
+        var celeb = $(this).attr("data-celeb");
+
+        // rendergifts(celeb)
+      
+        console.log(celeb);
+      
+        var gifQueryStr = "";
+        var gifQueryRating = "";
+        var gifQueryLimit;
+        var queryURL =
+          "https://api.giphy.com/v1/gifs/search?api_key=5H94PgMQxCIRZsyDmM1iOJzMsHdIhm8S&q=" +
+          celeb +
+          "&limit=" +
+          gifQueryLimit +
+          "&rating=" +
+          gifQueryRating;
+      
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          console.log(response.data);
+          var res = response.data;
+      
+          for (i = 0; i < res.length; i++) {
+            var newGIF = $("<img>");
+      
+            newGIF.addClass("gif-image");
+            // need image, which image in the images object?
+            newGIF.attr("src", res[i].images.downsized_still.url);
+      
+            // create attribute for new gif that is attached to the moving GIF
+            newGIF.attr("src-alt", res[i].images.downsized.url);
+      
+            $("#gif-display").append(newGIF);
+            // create css to format
+          }
+      
+          // Create an on click for still/moving GIF switch
+          $(".gif-image").on("click", function() {
+            var src_copy = $(this).attr("src");
+      
+            $(this).attr("src", $(this).attr("src-alt"));
+      
+            $(this).attr("src-alt", src_copy);
+          });
+        });
+        //
+      });
+
+  }
 
 
-// On search button click, run AJAX command to retrieve and API response
-$("#add-celeb-button").on("click", function(event) {
+// Call on the renderButtons function to initialize buttons from the stock list of celebs
+renderButtons();
+
+// // On search button click, run AJAX command to retrieve and API response
+$("#search-button").on("click", function(event) {
     // Prevent the form from submitting itself
     event.preventDefault();
 
-    // Create a variable called apiResponse 
-    var apiResponse;
+    // Grab the text from the search input box
+    var newCeleb = $("#celeb-input").val().trim();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
+    // Push this new celeb into the existing array of celebs
+    celebrities.push(newCeleb);
 
-    // add each search term to the topics 
+    console.log(celebrities);
+
+    // Run the renderButtons function to update the list of buttons on display to show the new button just added
+    renderButtons();
+    // renderGIFs(newCeleb);
 });
 
 
-// Save API response as a variable
 
-// Display the rating of each GIF under the respective GIF
+//     // Create a variable called apiResponse
+// var apiResponse;
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function(response) {
+//     console.log(response);
+// });
+
+// // Save API response as a variable
+
+// // Display the rating of each GIF under the respective GIF
