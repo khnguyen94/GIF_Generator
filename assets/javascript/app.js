6e5c2eeaec493bea6496820a7f08280bf0e57a21
Khoa Nguyen <kh.nguyen0101@gmail.com>
2019-10-02 12:12:55 -0700

    created independent renderGIFs function, added renderButton and renderGIFs function to on search click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,9 +9,6 @@
 // Create all variables
 var celebrities = ["Frank Ocean", "Kanye West", "The Weeknd", "Post Malone"];
 
-// Get a handle on the API Response
-var gifResponse;
-
 // Create a function that will render all the buttons listed in the topics array
 function renderButtons() {
   // Delete child nodes as to not repeat celebrity names
@@ -33,100 +30,131 @@ function renderButtons() {
     // add the newly created button to the HTML DOM
     $("#buttons-display").append(newCelebButton);
     console.log("celeb appended");
+
+    // Call the renderGIFs function on the newly added celeb/celeb button
   }
-
-    // Create an event listener for whenever the celeb name buttons are clicked
-    $(".button").on("click", function() {
-        event.preventDefault();
-      
-        // Clear the display 
-        $("#gif-display").empty();
-      
-        var celeb = $(this).attr("data-celeb");
-
-        // rendergifts(celeb)
-      
-        console.log(celeb);
-      
-        var gifQueryStr = "";
-        var gifQueryRating = "";
-        var gifQueryLimit;
-        var queryURL =
-          "https://api.giphy.com/v1/gifs/search?api_key=5H94PgMQxCIRZsyDmM1iOJzMsHdIhm8S&q=" +
-          celeb +
-          "&limit=" +
-          gifQueryLimit +
-          "&rating=" +
-          gifQueryRating;
-      
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          console.log(response.data);
-          var res = response.data;
-      
-          for (i = 0; i < res.length; i++) {
-            var newGIF = $("<img>");
-      
-            newGIF.addClass("gif-image");
-            // need image, which image in the images object?
-            newGIF.attr("src", res[i].images.downsized_still.url);
-      
-            // create attribute for new gif that is attached to the moving GIF
-            newGIF.attr("src-alt", res[i].images.downsized.url);
-      
-            $("#gif-display").append(newGIF);
-            // create css to format
-          }
-      
-          // Create an on click for still/moving GIF switch
-          $(".gif-image").on("click", function() {
-            var src_copy = $(this).attr("src");
-      
-            $(this).attr("src", $(this).attr("src-alt"));
-      
-            $(this).attr("src-alt", src_copy);
-          });
-        });
-        //
-      });
-
-  }
-
+}
 
 // Call on the renderButtons function to initialize buttons from the stock list of celebs
 renderButtons();
 
-// // On search button click, run AJAX command to retrieve and API response
-$("#search-button").on("click", function(event) {
-    // Prevent the form from submitting itself
-    event.preventDefault();
+// Create an event listener for whenever the celeb name buttons are clicked
+$(".button").on("click", function() {
+  event.preventDefault();
 
-    // Grab the text from the search input box
-    var newCeleb = $("#celeb-input").val().trim();
+  // Clear the display
+  $("#gif-display").empty();
 
-    // Push this new celeb into the existing array of celebs
-    celebrities.push(newCeleb);
+  var celeb = $(this).attr("data-celeb");
 
-    console.log(celebrities);
+  console.log(celeb);
 
-    // Run the renderButtons function to update the list of buttons on display to show the new button just added
-    renderButtons();
-    // renderGIFs(newCeleb);
+  // Call renderGIFs function on celeb: renderGIFs(celeb)
+  renderGIFs(celeb);
+
 });
 
+// // On search button click, dynamically create a new celeb button AND render the GIFs associated 
+$("#search-button").on("click", function(event) {
+  // Prevent the form from submitting itself
+  event.preventDefault();
 
+  // Grab the text from the search input box
+  var newCeleb = $("#celeb-input")
+    .val()
+    .trim();
 
-//     // Create a variable called apiResponse
-// var apiResponse;
+  // Push this new celeb into the existing array of celebs
+  celebrities.push(newCeleb);
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function(response) {
-//     console.log(response);
-// });
+  console.log(celebrities);
+
+  // Run the renderButtons function to update the list of buttons on display to show the new button just added
+  renderButtons();
+
+  // renderGIFs(newCeleb);
+  renderGIFs(newCeleb);
+});
+
+// Create a function that will dynamically render GIF cards and append them to the 
+function renderGIFs(celeb) {
+  var gifQueryLimit = 10;
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?api_key=5H94PgMQxCIRZsyDmM1iOJzMsHdIhm8S&q=" +
+    celeb +
+    "&limit=" +
+    gifQueryLimit;
+
+  // Create ajax api command
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    // should return the JSON dataframe as response
+    console.log(response.data); // console log just the data object from response
+
+    // Create a new variable called res and set it equal to that data object in response
+    var res = response.data;
+
+    // Create a for loop that goes throu every object within that data object
+    for (i = 0; i < res.length; i++) {
+      // It should create a a newGIFcard div, add class of card to it
+      var newGIFCard = $("<div>");
+
+      newGIFCard.addClass("card");
+
+      newGIFCard.attr("style", "width: 200px");
+
+      // Create a newGIFimg that is the card's image tag
+      // Assign it an attribute of srs and set it to a still GIF
+      // Assign it another attribute of src-alt and set it to a moving GIF
+      var newGIFimg = $("<img>");
+
+      newGIFimg.addClass("card-img-top");
+
+      newGIFimg.attr("src", res[i].images.downsized_still.url);
+
+      newGIFimg.attr("src-alt", res[i].images.downsized.url);
+
+      // Append the newGIFimg to newGIFcard
+      newGIFCard.append(newGIFimg);
+
+      // Create a new div for the card body
+      // Assign it a class of card-body
+      var newGIFCardBody = $("<div>");
+
+      newGIFCardBody.addClass("card-body");
+
+      // Append newGIFCardBody to newGIFCard
+      newGIFCard.append(newGIFCardBody);
+
+      // Create a new h4 tag with class card-title
+      // Add text for rating:
+      var newGIFCardTitle = $("<h4>");
+
+      newGIFCardTitle.addClass("card-title");
+
+      newGIFCardTitle.text("Rating: " + res[i].rating);
+
+      // Append the new card title to the card body
+      newGIFCardBody.append(newGIFCardTitle);
+
+      // Append it to the the gif-display in the HTML
+      $("#gif-display").append(newGIFCard);
+
+      // Now create css to format GIF cards
+    }
+
+    // Create an on click for still/moving GIF switch
+    $(".card-img-top").on("click", function() {
+      var src_copy = $(this).attr("src");
+
+      $(this).attr("src", $(this).attr("src-alt"));
+
+      $(this).attr("src-alt", src_copy);
+    });
+  });
+}
 
 // // Save API response as a variable
 
